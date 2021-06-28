7b9427c38d2e0e38597d044806c4d7daa39f2f23
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-06-28 11:03:23 -0300

    Update partition.cpp

--- partition/partition.cpp
+++ partition/partition.cpp
@@ -9,41 +9,56 @@
 
 using namespace std;
 
-int main ( void ){
+int main ()
+{
 
-  //Permite usar acentos
-  setlocale(LC_ALL,"");
+    //Permite usar acentos
+    setlocale(LC_ALL,"");
 
-  int vetor1[5], vetor2[5], vetor3[10];    // Declara um vetor de inteiros.
-  int i;
-      for (int i = 0 ; i < 6 ; ++i){
-        cout << "Entre com o valor para o vetor1 " << i << endl;
-          cin >> vetor1[i]; 
-      }      // Insere elementos no vetor1
+   char aux, pred;
+   int cont=0;
+   char lista[5];
 
-      for (int i = 0 ; i < 6 ; ++i){
-        cout << "Entre com o valor para o vetor2 " << i << endl;
-          cin >> vetor2[i]; 
-      } // Insere elementos no vetor2
-      
-      //Imprime vetor único na tela
-      for(int i = 0; i < 6; ++i){
-        for(int j = 0; j < 6; j++){
-          if( vetor1[i]  != vetor2[j]){
-            vetor3[i] = vetor1[i];
-            i++;
-            vetor3[i] = vetor2[j];
-          }else{
-            vetor3[i] = -1;
-          }
-        }
+   cout <<"Entre com o valor do predicado a ser testado." << endl;
+   cin >> pred;
+
+   cout <<"Entre com os elementos a serem testados." << endl;
+   for(int i = 0; i < 5; i++) {
+      cin >> lista[i];
+   }
+
+cout<<endl;
+
+for(int i = 0; i < 5; i++) {
+   for(int j = i+1; j < 5; j++)
+   {
+      if(lista[j] < lista[i]) {
+         aux = lista[i];
+         lista[i] = lista[j];
+         lista[j] = aux;
       }
+   }
+cont++;
+}
 
-      for(int i = 0; i < 11; i++){
-        cout << vetor3[i] << " ";
-      }
+cout <<"Lista com elementos menores que o predicado" << endl;
+for(int i = 0; i < 5; i++){
+    if(lista[i] < pred){
+        cout << lista[i] << " ";
+    }
+}
 
-      
+cout << endl;
+
+cout <<"Lista com elementos maiores que o predicado" << endl
+;
+for(int i = 0; i < 5; i++){
+    if(lista[i] > pred){
+        cout << lista[i] << " ";
+    }
+}
+
+cout << endl;
+
 return 0;
-} 
-  
+}
